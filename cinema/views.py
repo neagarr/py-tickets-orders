f43b94fbb39d09eeac8adb2d65fbab0c64c5448e
Mykola Kokoiev <veokoknik@gmail.com>
2025-03-24 11:21:59 +0100

    add TicketSerializer, OrderSerializer & add TicketViewSet, OrderViewSet & add router for orders

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
+from django.http import request
 from rest_framework import viewsets
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
 
 from cinema.serializers import (
     GenreSerializer,
@@ -11,7 +12,7 @@ from cinema.serializers import (
     MovieSessionListSerializer,
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
-    MovieListSerializer,
+    MovieListSerializer, OrderSerializer, TicketSerializer,
 )
 
 
@@ -67,3 +68,20 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return self.queryset.select_related("movie", "cinema_hall")
 
         return self.queryset
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        if self.action == "list":
+            return self.queryset.prefetch_related(
+                "tickets__movie_session__movie",
+                "tickets__movie_session__cinema_hall"
+            )
